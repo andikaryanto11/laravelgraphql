102b10ba8afbf5376e57b23ce69fd360c1fa3bcc
Andik <andik.aryantoo@gmail.com>
2023-11-11 23:44:41 +0700

    - filter input

---
+++ src/Inputs/FilterInput.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace LaravelGraphQL\Inputs;
+
+class FilterInput extends AbstractInput
+{
+    protected string $search = '';
+
+    public function __construct()
+    {
+    }
+
+    /**
+     * Get the value of search
+     */ 
+    public function getSearch()
+    {
+        return $this->search;
+    }
+}
